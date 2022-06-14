e83552e284fd8767c5f2e236079e9188dbef2f36
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-13 22:05:13 -0300

    add greeting function

--- print-message.js
+++ print-message.js
@@ -1,3 +1,12 @@
-const { messages } = require("./new-messages")
+const { messages } = require("./new-messages");
 
-messages.map((message, i) => console.log(`${i}: ${message}`))
+const localHour = new Date().getHours();
+const generateGreeting = (hour) => {
+  if (hour >= 18 || hour < 6) return "Boa Noite!";
+  if (hour >= 18 && hour < 6) return "Bom Dia!";
+  if (hour >= 12 && hour < 18) return "Boa Tarde!";
+  return "...";
+};
+const greeting = generateGreeting(localHour);
+console.log(greeting);
+messages.map((message, i) => console.log(`${i}: ${message}`));
